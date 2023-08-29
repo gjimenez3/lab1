251df55c5d98518fdad38c8bf9341b805da92806
Gustavo Jimenez <gjimenez3@odin.cs.csubak.edu>
2023-08-29 07:52:54 -0700

    first commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+all:tuesday
+	
+tuesday: tuesday.cpp
+	g++ tuesday.cpp -Wall - o tuesday
+
+clean:
+	rm - f tuesday 
---
+++ tuesday.cpp
@@ -0,0 +1,9 @@
+//gustavo jimenez
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+    cout <<"hello world" << endl;
+    return 0;
+   }
